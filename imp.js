0a64a0c0170d10f081d1c8ec2b582d93b947b3c2
aedeng28 <aedengalang@gmail.com>
2026-07-16 17:54:02 +0800

    Add ct function to update innerHTML of an element

--- imp.js
+++ imp.js
@@ -6,3 +6,7 @@ function change(input, text){
     const inv = document.getElementById(input);
     inv.value = text;
 };
+function ct(id, new){
+    const inv = document.getElementById(input);
+    inv.innerHTML = new;
+};
