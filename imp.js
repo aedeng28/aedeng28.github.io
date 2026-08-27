b4464c6e963fb2d16be3a256eb18e0953f5d7d8d
aedeng28 <aedengalang@gmail.com>
2026-08-27 18:42:21 +0800

    Update imp.js

--- imp.js
+++ imp.js
@@ -1,12 +1,12 @@
-function getinput(input){
+export function getinput(input){
     const inn = document.getElementById(input);
     return inn.value;
 };
-function change(input, text){
+export function change(input, text){
     const inv = document.getElementById(input);
     inv.value = text;
 };
-function ct(id, new){
+export function ct(id, new){
     const inv = document.getElementById(input);
     inv.innerHTML = new;
 };
